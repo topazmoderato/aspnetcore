3df468e9f097773ce7c58ee8950870420c26b049
Hao Kung <HaoK@users.noreply.github.com>
2021-06-29 15:56:58 -0700

    Fix public api

--- src/Identity/ApiAuthorization.IdentityServer/src/Options/IdentityResourceBuilder.cs
+++ src/Identity/ApiAuthorization.IdentityServer/src/Options/IdentityResourceBuilder.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -12,7 +12,7 @@ namespace Microsoft.AspNetCore.ApiAuthorization.IdentityServer
     /// </summary>
     public class IdentityResourceBuilder
     {
-        private IdentityResource _identityResource;
+        private readonly IdentityResource _identityResource;
         private bool _built;
 
         /// <summary>
